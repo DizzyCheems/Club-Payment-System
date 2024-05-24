08b3e906950281543c0e595ae1678d4fab81c085
DizzyCheems <stine6595@gmail.com>
2024-05-24 22:02:46 +0800

    for checking

--- resources/views/User/dashboard.blade.php
+++ resources/views/User/dashboard.blade.php
@@ -34,56 +34,35 @@
 				    </div><!--//inner-->
 			    </div><!--//app-card-->
 				    
-			    <div class="row g-4 mb-4">
-				    <div class="col-6 col-lg-3">
-					    <div class="app-card app-card-stat shadow-sm h-100">
-						    <div class="app-card-body p-3 p-lg-4">
-							    <h4 class="stats-type mb-1">Net Paid</h4>
-							    <div class="stats-figure">₱{{ $totalAmount }}</div>
-							    <div class="stats-meta text-success">
-								    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-</svg> 0%</div>
-						    </div><!--//app-card-body-->
-						    <a class="app-card-link-mask" href="#"></a>
-					    </div><!--//app-card-->
-				    </div><!--//col-->
-				    
-				    <div class="col-6 col-lg-3">
-					    <div class="app-card app-card-stat shadow-sm h-100">
-						    <div class="app-card-body p-3 p-lg-4">
-							    <h4 class="stats-type mb-1">Net Pending Payment</h4>
-							    <div class="stats-figure">₱0</div>
-							    <div class="stats-meta text-success">
-								    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-</svg> 0% </div>
-						    </div><!--//app-card-body-->
-						    <a class="app-card-link-mask" href="#"></a>
-					    </div><!--//app-card-->
-				    </div><!--//col-->
-				    <div class="col-6 col-lg-3">
-					    <div class="app-card app-card-stat shadow-sm h-100">
-						    <div class="app-card-body p-3 p-lg-4">
-							    <h4 class="stats-type mb-1">Events/Agendas</h4>
-							    <div class="stats-figure">{{ $agendaCount }}</div>
-							    <div class="stats-meta">
-								    Open</div>
-						    </div><!--//app-card-body-->
-						    <a class="app-card-link-mask" href="#"></a>
-					    </div><!--//app-card-->
-				    </div><!--//col-->
-				    <div class="col-6 col-lg-3">
-					    <div class="app-card app-card-stat shadow-sm h-100">
-						    <div class="app-card-body p-3 p-lg-4">
-							    <h4 class="stats-type mb-1">Invoices</h4>
-							    <div class="stats-figure">0</div>
-							    <div class="stats-meta">New</div>
-						    </div><!--//app-card-body-->
-						    <a class="app-card-link-mask" href="#"></a>
-					    </div><!--//app-card-->
-				    </div><!--//col-->
-			    </div><!--//row-->
+				<div class="row g-4 mb-4">
+    <div class="col-12 col-lg-6">
+        <div class="app-card app-card-stat shadow-sm h-100">
+            <div class="app-card-body p-3 p-lg-4">
+                <h4 class="stats-type mb-1">Net Paid</h4>
+                <div class="stats-figure">₱{{ $totalAmount }}</div>
+                <div class="stats-meta text-success">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+                    </svg> 0%
+                </div>
+            </div><!--//app-card-body-->
+            <a class="app-card-link-mask" href="#"></a>
+        </div><!--//app-card-->
+    </div><!--//col-->
+
+    <div class="col-12 col-lg-6">
+        <div class="app-card app-card-stat shadow-sm h-100">
+            <div class="app-card-body p-3 p-lg-4">
+                <h4 class="stats-type mb-1">Events/Agendas</h4>
+                <div class="stats-figure">{{ $agendaCount }}</div>
+                <div class="stats-meta">
+                    Open
+                </div>
+            </div><!--//app-card-body-->
+            <a class="app-card-link-mask" href="#"></a>
+        </div><!--//app-card-->
+    </div><!--//col-->
+</div><!--//row-->
 
 			        
 		
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -86,114 +86,49 @@
 			    </div><!--//row-->
 
 			        
-		
-			    <div class="row g-4 mb-4">
-				    <div class="col-12 col-lg-6">
-				        <div class="app-card app-card-progress-list h-100 shadow-sm">
-					        <div class="app-card-header p-3">
-						        <div class="row justify-content-between align-items-center">
-							        <div class="col-auto">
-						                <h4 class="app-card-title">Budget Progress</h4>
-							        </div><!--//col-->
-							        <div class="col-auto">
-								        <div class="card-header-action">
-									        <a href="{{route('agenda.index')}}">All Agendas</a>
-								        </div><!--//card-header-actions-->
-							        </div><!--//col-->
-						        </div><!--//row-->
-								</div><!--//app-card-header-->
-									<div class="app-card-body">
-										@foreach($recentAgendas as $agenda)
-											<div class="item p-3">
-												<div class="row align-items-center">
-													<div class="col">
-														<div class="title mb-1">{{ $agenda->agenda_name }}</div>
-														<div class="progress">
-															@php
-																$progressPercentage = $agenda->total_payments_amount / $agenda->total_fund * 100;
-															@endphp
-															<div class="progress-bar bg-success" role="progressbar" style="width: {{ $progressPercentage }}%;" aria-valuenow="{{ $progressPercentage }}" aria-valuemin="0" aria-valuemax="100"></div>
-														</div>
-													</div><!--//col-->
-													<div class="col-auto">
-														<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-															<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-														</svg>
-													</div><!--//col-->
-												</div><!--//row-->
-												<a class="item-link-mask" href="#"></a>
-											</div><!--//item-->
-										@endforeach
-									</div><!--//app-card-body-->
-				        </div><!--//app-card-->
-			        </div><!--//col-->
-			        <div class="col-12 col-lg-6">
-				        <div class="app-card app-card-stats-table h-100 shadow-sm">
-					        <div class="app-card-header p-3">
-						        <div class="row justify-content-between align-items-center">
-							        <div class="col-auto">
-						                <h4 class="app-card-title">Payment Overview</h4>
-							        </div><!--//col-->
-							        <div class="col-auto">
-								        <div class="card-header-action">
-									        <a href="#">View report</a>
-								        </div><!--//card-header-actions-->
-							        </div><!--//col-->
-						        </div><!--//row-->
-					        </div><!--//app-card-header-->
-					        <div class="app-card-body p-3 p-lg-4">
-						        <div class="table-responsive">
-							        <table class="table table-borderless mb-0">
-										<thead>
-											<tr>
-												<th class="meta">Source</th>
-												<th class="meta stat-cell">Views</th>
-												<th class="meta stat-cell">Today</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td><a href="#">BSCS 3</a></td>
-												<td class="stat-cell">110</td>
-												<td class="stat-cell">
-													<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up text-success" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-									  <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-									</svg> 
-									                30%
-									            </td>
-											</tr>
-											<tr>
-												<td><a href="#">BSCS 2</a></td>
-												<td class="stat-cell">67</td>
-												<td class="stat-cell">23%</td>
-											</tr>
-											<tr>
-												<td><a href="#">BSCS 1</a></td>
-												<td class="stat-cell">56</td>
-												<td class="stat-cell">
-													<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down text-danger" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-									  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-									</svg>
-												    20%
-											    </td>
-											</tr>
-											<tr>
-												<td><a href="#">IT 3 </a></td>
-												<td class="stat-cell">24</td>
-												<td class="stat-cell">-</td>
-											</tr>
-											<tr>
-												<td><a href="#">BSIT 2 </a></td>
-												<td class="stat-cell">17</td>
-												<td class="stat-cell">15%</td>
-											</tr>
-										</tbody>
-									</table>
-						        </div><!--//table-responsive-->
-					        </div><!--//app-card-body-->
-				        </div><!--//app-card-->
-			        </div><!--//col-->
-			    </div><!--//row-->
+				<div class="row g-4 mb-4">
+    <div class="col-12">
+        <div class="app-card app-card-progress-list h-100 shadow-sm">
+            <div class="app-card-header p-3">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-auto">
+                        <h4 class="app-card-title">Budget Progress</h4>
+                    </div><!--//col-->
+                    <div class="col-auto">
+                        <div class="card-header-action">
+                            <a href="{{route('agenda.index')}}">All Agendas</a>
+                        </div><!--//card-header-actions-->
+                    </div><!--//col-->
+                </div><!--//row-->
+            </div><!--//app-card-header-->
+            <div class="app-card-body">
+                @foreach($recentAgendas as $agenda)
+                <div class="item p-3">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <div class="title mb-1">{{ $agenda->agenda_name }}</div>
+                            <div class="progress">
+                                @php
+                                $progressPercentage = $agenda->total_payments_amount / $agenda->total_fund * 100;
+                                @endphp
+                                <div class="progress-bar bg-success" role="progressbar" style="width: {{ $progressPercentage }}%;" aria-valuenow="{{ $progressPercentage }}" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div><!--//col-->
+                        <div class="col-auto">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        </div><!--//col-->
+                    </div><!--//row-->
+                    <a class="item-link-mask" href="#"></a>
+                </div><!--//item-->
+                @endforeach
+            </div><!--//app-card-body-->
+        </div><!--//app-card-->
+    </div><!--//col-->
+</div><!--//row-->
+
+			     
 			    <div class="row g-4 mb-4">
 				    <div class="col-12 col-lg-4">
 					    <div class="app-card app-card-basic d-flex flex-column align-items-start shadow-sm">
